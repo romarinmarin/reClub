16351ba5ceb25a6acbe65c356ed11985eb3941ed
Romaric Anquetil <romaricanquetil@mbp-de-romaric.lan>
2019-01-09 16:19:00 +0100

    refactoring Stripes animations

--- src/Components/Home/Featured/Stripes.js
+++ src/Components/Home/Featured/Stripes.js
@@ -29,47 +29,54 @@ class Stripes extends Component {
     ]
   };
 
-  showStripes = this.state.stripes.map((stripe, i) => (
-    <Animate
-      key={i}
-      show={true}
-      start={{
-        background: "#ffffff",
-        opacity: 0,
-        left: 0,
-        rotate: 0,
-        top: 0
-      }}
-      enter={{
-        background: [stripe.background],
-        opacity: [1],
-        left: [stripe.left],
-        rotate: [stripe.rotate],
-        top: [stripe.top],
-        timing: { delay: stripe.delay, duration: 2000, ease: easePolyOut },
-        events: {
-          end() {
-            console.log("animation finished");
-          }
-        }
-      }}
-    >
-      {({ opacity, left, rotate, top, background }) => {
-        return (
-          <div
-            className="stripe"
-            style={{
-              background,
-              opacity,
-              transform: `rotate(${rotate}deg) translate(${left}px,${top}px)`
-            }}
-          />
-        );
-      }}
-    </Animate>
-  ));
+  renderStripes = () => {
+    return this.state.stripes.map((stripe, i) => {
+      let show = true;
+      let { background, top, left, rotate, delay } = stripe;
+      return (
+        <Animate
+          key={i}
+          show={show}
+          start={{
+            backgroundColor: "fff",
+            left: 0,
+            top: 0,
+            rotate: 0,
+            opacity: 0
+          }}
+          enter={{
+            backgroundColor: [background],
+            left: [left],
+            top: [top],
+            timing: { delay: delay, duration: 200, ease: easePolyOut },
+            opacity: 1,
+            rotate: [rotate],
+            events: {
+              end() {
+                console.log("animation finished");
+              }
+            }
+          }}
+        >
+          {({ opacity, backgroundColor, top, left, rotate }) => {
+            return (
+              <div
+                className="stripe"
+                style={{
+                  backgroundColor: backgroundColor,
+                  opacity,
+                  transform: `rotate(${rotate}deg) translate(${left}px,${top}px)`
+                }}
+              />
+            );
+          }}
+        </Animate>
+      );
+    });
+  };
+
   render() {
-    return <div className="featured_stripes">{this.showStripes}</div>;
+    return <div className="featured_stripes">{this.renderStripes()}</div>;
   }
 }
 
