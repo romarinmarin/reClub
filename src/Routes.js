8eadc1055e4669f87930fa61f37fd8d49f9559e7
Romaric <romaric.anquetil@gmail.com>
2019-02-13 11:28:51 +0100

    add PublicRoutes

--- src/Routes.js
+++ src/Routes.js
@@ -4,14 +4,27 @@ import Home from "./Components/Home/Home";
 import { Switch, Route } from "react-router-dom";
 import SignIn from "./Components/SignIn";
 import Dashboard from "./Components/Admin/Dashboard";
+import PublicRoutes from "./Components/AuthRoutes/PublicRoutes";
 
-const Routes = () => {
+const Routes = props => {
   return (
     <Layout>
       <Switch>
-        <Route exact path="/" component={Home} />
         <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/the_matches" component={SignIn} />
+        <PublicRoutes
+          restricted={false}
+          {...props}
+          exact
+          path="/"
+          component={Home}
+        />
+        <PublicRoutes
+          restricted={true}
+          {...props}
+          exact
+          path="/sign_in"
+          component={SignIn}
+        />
       </Switch>
     </Layout>
   );
